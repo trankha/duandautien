8fe9d3e726cd22933b33cc67c82cd8266df0db8e
luanvantck <chikha12c2@gmail.com>
2017-03-30 06:32:06 +0700

    Cap nhat code lai web tinh github

--- Temp/expert.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var expertRouter = express.Router();
-var thresholdController = require('../controllers/expert/thresholdController');
-var stockingdetailController = require('../controllers/expert/stockingdetailController');
-var trackeraugumentedController = require('../controllers/expert/trackeraugumentedController');
-expertRouter.use('/nguong',thresholdController);
-expertRouter.use('/nhatky',stockingdetailController);
-expertRouter.use('/theodoitangtruong',trackeraugumentedController);
-module.exports = expertRouter;
